04bf262877b9c1665e717b35a9e56a5b8bad3504
bob <bobmccarthyatx@gmail.com>
2015-09-22 16:39:47 -0500

    fixed hamburger button

---
+++ scripts/maps.js
@@ -0,0 +1,12 @@
+'use strict';
+// var map = maps(google.maps.Map(mapCanvas, mapOptions));
+
+// map.addEventListener('load', function initialize() {
+	
+//     var mapCanvas = document.getElementById('map');
+//     var mapOptions = {
+//         center: new google.maps.LatLng(44.5403, -78.5463),
+//         zoom: 8,
+//         mapTypeId: google.maps.MapTypeId.ROADMAP
+//     }  
+// }
